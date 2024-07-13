4695d35a8405b468cdccef64b4be54a77ef43c29
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-07-13 11:42:08 +0300

    m

--- api/app.py
+++ api/app.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template, request, session, redirect
 from supabase import create_client
+from werkzeug.security import check_password_hash, generate_password_hash
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 app.secret_key ='secret_key'
 
-supabase_url = "https://sbvcrvdxmvloiowjjmay.supabase.co"
-supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNidmNydmR4bXZsb2lvd2pqbWF5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDY0MTA5MCwiZXhwIjoyMDM2MjE3MDkwfQ.b4InyfVM_vLFuKnCTiu-f8e0LGkR_G8RGHQKnVxlwF4"
+supabase_url = getenv('SUPABASE_URL')
+supabase_key = getenv('SUPABASE_KEY')
 supabase_client = create_client(supabase_url, supabase_key)
 
 @app.route('/')
 def home():
     if 'username' in session:
-        return render_template('index.html')
+        groups = supabase_client.table("messages").select('*').execute().data
+        messages = supabase_client.table("messages").select('*').execute().data
+        return render_template('index.html', username=session["username"], messages=messages, groups=groups)
     return redirect('/login')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -20,13 +27,26 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = supabase_client.table('users').select('username,password').eq('username', username).eq('password', password).execute()
-        if user[0]:
-            session['username'] = username
-            return redirect('/')
-    return render_template('login.html')
+        user = supabase_client.table('users').select('username,password').eq('username', username).execute().data
+        if len(user) > 0:
+            if check_password_hash(user[0]['password'], password):
+                session['username'] = username
+                return redirect('/')
+            return render_template('login.html', errorMsg="Invalid password")
+        return render_template('login.html', errorMsg="User not found")
+    return render_template('login.html', errorMsg="")
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        user = supabase_client.table('users').select("username").eq('username', username).execute().data
+        if len(user) > 0:
+            return render_template('register.html', errorMsg="Username already exists")
+        password = request.form['password']
+        supabase_client.table('users').insert({"username": username, "password": generate_password_hash(password)}).execute()
+        return redirect('/login')
+    return render_template('register.html', errorMsg="")
+
+
 
