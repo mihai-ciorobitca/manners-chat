940cfc2eae01f2662669e3a58e11a3adcd475b66
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-07-13 12:03:50 +0300

    m

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from supabase import create_client
 from werkzeug.security import check_password_hash, generate_password_hash
 from dotenv import load_dotenv
 from os import getenv
+from requests import post
 
 load_dotenv()
 
@@ -12,6 +13,7 @@ app.secret_key ='secret_key'
 
 supabase_url = getenv('SUPABASE_URL')
 supabase_key = getenv('SUPABASE_KEY')
+recaptcha_secret = getenv('RECAPTCHA_SECRET')
 supabase_client = create_client(supabase_url, supabase_key)
 
 @app.route('/')
@@ -40,13 +42,20 @@ def login():
 def register():
     if request.method == 'POST':
         username = request.form['username']
+        recaptcha_response = request.form['g-recaptcha-response']
+        captcha_data = {
+            'secret': recaptcha_secret,
+            'response': recaptcha_response
+        }
+        captcha_verify = post('https://www.google.com/recaptcha/api/siteverify', data=captcha_data)
+        captcha_response = captcha_verify.json()
+        if not captcha_response['success']:
+            return render_template('register.html', errorMsg="reCAPTCHA verification failed. Please try again.")
         user = supabase_client.table('users').select("username").eq('username', username).execute().data
         if len(user) > 0:
             return render_template('register.html', errorMsg="Username already exists")
+
         password = request.form['password']
         supabase_client.table('users').insert({"username": username, "password": generate_password_hash(password)}).execute()
-        return redirect('/login')
+        return redirect('/login')   
     return render_template('register.html', errorMsg="")
-
-
-
